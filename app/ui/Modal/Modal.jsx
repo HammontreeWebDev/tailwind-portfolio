4e59bbce4e1efa1e6672e63d698ad155e14a4c0e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-10-23 09:58:58 -0400

    base modal added

---
+++ app/ui/Modal/Modal.jsx
@@ -0,0 +1,51 @@
+import { Dialog } from '@headlessui/react';
+import Image from 'next/image';
+
+export default function Modal({ title, content, leftButton, rightButton, onClose, imagesrc, imagealt }) {
+    return (
+        <Dialog open={true} onClose={onClose} className="relative z-10">
+            <div className="fixed inset-0 bg-[var(--alt4)] bg-opacity-75" />
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-black px-4 pb-4 pt-5 text-left shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                        <div>
+                            <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full">
+                                <Image
+                                    src={imagesrc}
+                                    alt={imagealt}
+                                    height={50}
+                                    width={50}
+                                />
+                            </div>
+                            <div className="mt-3 text-center sm:mt-5">
+                                <Dialog.Title as="h3" className="title-text text-base font-semibold leading-6 text-[var(--primary2)]">
+                                    {title}
+                                </Dialog.Title>
+                                <div className="mt-2">
+                                    <p className="text-sm text-white">{content}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                            <button
+                                type="button"
+                                onClick={onClose}
+                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-50"
+                            >
+                                {leftButton}
+                            </button>
+                            <button
+                                type="button"
+                                onClick={onClose}
+                                className="mt-3 inline-flex w-full justify-center rounded-md bg-[var(--primary5)] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[var(--primary4)]"
+                            >
+                                {rightButton}
+                            </button>
+                        </div>
+                    </Dialog.Panel>
+                </div>
+            </div>
+        </Dialog>
+    );
+}
--- app/ui/TechStack/TechStack.jsx
+++ app/ui/TechStack/TechStack.jsx
@@ -1,134 +1,78 @@
+"use client";
+
 import Image from "next/image";
+import Modal from "../Modal/Modal";
+import { useState } from "react";
 
 export default function TechStack() {
+    // State to track which modal is open
+    const [openModalIndex, setOpenModalIndex] = useState(null);
+
+    // Array of tech stack data for simplicity
+    const techStack = [
+        { name: "Apollo", src: "/img/techStack/Apollo.svg" },
+        { name: "CSS3", src: "/img/techStack/css3.svg" },
+        { name: "ExpressJS", src: "/img/techStack/ExpressJS.svg" },
+        { name: "Figma", src: "/img/techStack/figma.svg" },
+        { name: "Github Pages", src: "/img/techStack/ghPages.svg" },
+        { name: "GraphQL", src: "/img/techStack/GraphQL.svg" },
+        { name: "HTML5", src: "/img/techStack/html5.svg" },
+        { name: "JavaScript", src: "/img/techStack/javascript.svg" },
+        { name: "jQuery", src: "/img/techStack/jQuery.svg" },
+        { name: "MongoDB", src: "/img/techStack/MongoDB.svg" },
+        { name: "mySQL", src: "/img/techStack/mySQL.svg" },
+        { name: "NodeJS", src: "/img/techStack/NodeJS.svg" },
+        { name: "ReactJS", src: "/img/techStack/ReactJS.svg" },
+        { name: "SQL", src: "/img/techStack/sql.svg" },
+        { name: "Vercel", src: "/img/techStack/vercel.svg" },
+    ];
+
+    const handleModalOpen = (index) => {
+        setOpenModalIndex(index);
+    };
+
+    const handleModalClose = () => {
+        setOpenModalIndex(null);
+    };
+
     return (
-        <div className="bg-[#0a0a0a] py-24 sm:py-32">
+        <div className="bg-[var(--primary4)] py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <h2 className="text-center text-lg font-semibold leading-8 text-white">
-                    Your next project could be built using. . .
+                <h2 className="title-text text-center text-lg font-semibold leading-8 text-white">
+                    Your next project could be built using...
                 </h2>
-
-                {/* //! Row 1 */}
-
-                <div className="mx-auto mt-10 flex flex-wrap justify-evenly items-center gap-x-8 gap-y-10 max-w-lg sm:max-w-xl lg:mx-0 lg:max-w-none">
-                    <Image
-                        alt="Apollo"
-                        src="/img/techStack/Apollo.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Reform"
-                        src="/img/techStack/css3.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Tuple"
-                        src="/img/techStack/ExpressJS.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="SavvyCal"
-                        src="/img/techStack/figma.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Statamic"
-                        src="/img/techStack/ghPages.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                </div>
-
-                {/* //! Row 2 */}
+                <p className="text-center text-sm italic font-bold leading-8 text-[var(--primary2)]">
+                    (Click the icons to learn more)
+                </p>
 
                 <div className="mx-auto mt-10 flex flex-wrap justify-evenly items-center gap-x-8 gap-y-10 max-w-lg sm:max-w-xl lg:mx-0 lg:max-w-none">
-                    <Image
-                        alt="Apollo"
-                        src="/img/techStack/GraphQL.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Reform"
-                        src="/img/techStack/html5.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Tuple"
-                        src="/img/techStack/javascript.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="SavvyCal"
-                        src="/img/techStack/jQuery.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Statamic"
-                        src="/img/techStack/MongoDB.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
+                    {techStack.map((tech, index) => (
+                        <div key={index} className="flex flex-col items-center">
+                            <Image
+                                alt={tech.name}
+                                src={tech.src}
+                                width={100}
+                                height={100}
+                                className="object-contain cursor-pointer"
+                                onClick={() => handleModalOpen(index)} // Open the modal for the clicked image
+                            />
+
+                            {/* Conditionally render the modal for the clicked image */}
+                            {openModalIndex === index && (
+                                <Modal
+                                    title={tech.name}
+                                    content={`Would you like to learn more about ${tech.name}?`}
+                                    leftButton="Exit"
+                                    rightButton="Learn More"
+                                    onClose={handleModalClose} // Close the modal when either button is clicked
+                                    imagesrc={tech.src}
+                                    imagealt={tech.name}
+                                />
+                            )}
+                        </div>
+                    ))}
                 </div>
-
-                {/* //! Row 3 */}
-
-                <div className="mx-auto mt-10 flex flex-wrap justify-evenly items-center gap-x-8 gap-y-10 max-w-lg sm:max-w-xl lg:mx-0 lg:max-w-none">
-                    <Image
-                        alt="Apollo"
-                        src="/img/techStack/mySQL.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Reform"
-                        src="/img/techStack/NodeJS.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Tuple"
-                        src="/img/techStack/ReactJS.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="SavvyCal"
-                        src="/img/techStack/sql.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                    <Image
-                        alt="Statamic"
-                        src="/img/techStack/vercel.svg"
-                        width={100}
-                        height={100}
-                        className="object-contain"
-                    />
-                </div>
-
             </div>
         </div>
-    )
+    );
 }
