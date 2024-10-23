63c976e3a939e08b5a88b69ac8902d3044ef1686
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-10-22 22:18:24 -0400

    added testimonial component

--- app/page.js
+++ app/page.js
@@ -1,17 +1,29 @@
 import HeroImage from "./ui/heroImage/HeroImage";
+import Testimonial from "./ui/testimonial/Testimonial";
 
 export default function Home() {
   return (
-    <HeroImage
-    title={"Transforming Visions Into Digital Reality"}
-    subText={"From dynamic, full-scale applications to professional front-end websites, Hammontree Full-Stack Solutions is here to bring your ideas to life."}
-    buttonText={"Get started"}
-    image1={"/img/home/brofit.png"}
-    image2={"/img/home/jesse.png"}
-    image3={"/img/home/outtaTouch.png"}
-    image4={"/img/home/pokewire.png"}
-    image5={"/img/home/toBeWith.png"}
-    btnHref={"/contact"}
-    />
+    <>
+
+      <HeroImage
+        title={"Transforming Visions Into Digital Reality"}
+        subText={"From dynamic, full-scale applications to professional front-end websites, Hammontree Full-Stack Solutions is here to bring your ideas to life."}
+        buttonText={"Get started"}
+        image1={"/img/home/brofit.png"}
+        image2={"/img/home/jesse.png"}
+        image3={"/img/home/outtaTouch.png"}
+        image4={"/img/home/pokewire.png"}
+        image5={"/img/home/toBeWith.png"}
+        btnHref={"/contact"}
+      />
+
+      <Testimonial
+        quote={"Awesome company, and made my life so much easier!"}
+        client={"Reese Wilder"}
+        occupation={"Owner of BroFit Training"}
+        picture={"/img/home/brofitLogo.webp"}
+      />
+
+    </>
   );
 }
